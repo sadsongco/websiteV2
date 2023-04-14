b995ad187c453aeeb76a6649ebe10a85d225a83a
Nigel Powell <nigel@thesadsongco.com>
2023-04-14 10:47:55 +0100

    Text size for mobile

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,6 +1,6 @@
 const navList = document.getElementById('nav-bar').getElementsByTagName('li');
 for (const liItem of navList) {
   liItem.onclick = () => {
-    document.getElementById(liItem.getElementsByTagName('a')[0].dataset.target).scrollIntoView();
+    document.getElementById(liItem.getElementsByTagName('a')[0].dataset.target).scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'center' });
   };
 }
