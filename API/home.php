231d35f3cdf5f3d37673565917aed1822de42db9
Nigel Powell <nigel@thesadsongco.com>
2023-04-17 10:04:36 +0100

    API init commit

---
+++ API/home.php
@@ -0,0 +1,7 @@
+<?php
+
+$output = array( "item" => 1, "text" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum at vel ea quae aperiam a iusto facilis perspiciatis expedita velit, quam corrupti. Excepturi nam temporibus asperiores magni molestiae? Obcaecati, distinctio?");
+
+echo json_encode($output);
+
+?>
