5bc4ed01a387fedd9934037d2c213c4105fca9c8
Nigel Powell <nigel@thesadsongco.com>
2023-04-18 10:42:17 +0100

    Frosted glass design, tweaks

--- src/API/home.php
+++ src/API/home.php
@@ -3,11 +3,15 @@
 $output = array(
     array(
         "target" => "other",
-        "content" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum at vel ea quae aperiam a iusto facilis perspiciatis expedita velit, quam corrupti. Excepturi nam temporibus asperiores magni molestiae? Obcaecati, distinctio?"
+        "content" => "OTHER :::: Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum at vel ea quae aperiam a iusto facilis perspiciatis expedita velit, quam corrupti. Excepturi nam temporibus asperiores magni molestiae? Obcaecati, distinctio?"
     ),
     array(
         "target" => "about",
-        "content" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti voluptatem soluta sequi enim omnis cupiditate. Voluptatum, quos aut libero a obcaecati expedita nam, facere adipisci eligendi debitis velit iusto tenetur?"
+        "content" => "ABOUT ::::Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti voluptatem soluta sequi enim omnis cupiditate. Voluptatum, quos aut libero a obcaecati expedita nam, facere adipisci eligendi debitis velit iusto tenetur?"
+    ),
+    array(
+        "target" => "album",
+        "content" => "ALBUM :::: Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia dicta unde voluptatibus earum adipisci expedita non veritatis dolorem? Harum, earum voluptatibus? Maxime iure velit voluptates quisquam amet ea culpa, eos enim. Modi sapiente qui cumque assumenda sunt? Officiis nobis repellendus dicta neque similique voluptatum ab facilis eaque obcaecati ut temporibus in, asperiores eveniet nihil, corporis odio quae alias.\n\nQuia, facilis, sit expedita voluptatem illo, quasi eos est ad laudantium reiciendis ex! Delectus odit quae sapiente, aliquid asperiores laboriosam eveniet officiis, cupiditate, id deleniti earum itaque in eligendi maxime nobis reprehenderit doloribus! Vel aliquid non quo eum eius in consequatur consectetur error nemo perspiciatis repudiandae nesciunt.\n\n Magni voluptas dignissimos dolore quibusdam praesentium natus et ipsum dolorem quas labore? Delectus voluptatibus quibusdam maxime iure dolorem aliquid illo, velit corporis excepturi tempore voluptas laborum tempora voluptatem! Porro quaerat nulla quidem minus eveniet error repudiandae numquam ad quos aliquid quod ab dolores facere assumenda eaque quam, ipsum ea unde. Iste earum sunt tempore ab libero eius quae suscipit iure quas corrupti, necessitatibus consectetur impedit rerum aliquam neque animi sit nobis nam quam sequi natus explicabo doloremque.\n\nEa, expedita nemo. Repudiandae perspiciatis dolorum, animi aliquam reprehenderit optio dolore minus cumque temporibus assumenda dolor. Dolorem, voluptas!"
     )
 );
 
--- src/js/content.js
+++ src/js/content.js
@@ -1,6 +1,45 @@
 // grab loader element to use where needed
 const loader = document.getElementById('loading');
 
+// load background images and show page
+
+const loadBackgroundImage = async (src) => {
+  return new Promise((resolve, reject) => {
+    const image = new Image();
+    image.addEventListener('load', resolve);
+    image.addEventListener('error', reject);
+    image.src = src;
+    return image;
+  });
+};
+
+const placeBackgroundImage = async (src, target) => {
+  target.style.backgroundImage = `url(${src})`;
+};
+
+const buildBackgrounds = async () => {
+  const backgrounds = {
+    about: 'https://picsum.photos/1000',
+    album: 'https://picsum.photos/800',
+    shows: 'https://picsum.photos/900',
+    other: 'https://picsum.photos/950',
+  };
+  const cards = document.getElementsByClassName('card');
+  let backgroundImagePromises = [];
+  for (const card of cards) {
+    backgroundImagePromises.push(loadBackgroundImage(backgrounds[card.id.replace('-container', '')]));
+  }
+  await Promise.all(backgroundImagePromises);
+  for (const backgroundKey in backgrounds) {
+    const target = document.getElementById(`${backgroundKey}-container`);
+    await placeBackgroundImage(backgrounds[backgroundKey], target);
+  }
+  document.getElementById('initial-load').style.display = 'none';
+  loader.style.display = 'none';
+};
+
+// load page content and display
+
 const showLoading = async (targetId) => {
   const newLoader = loader.cloneNode(true);
   newLoader.id = `${targetId}-loader`;
@@ -12,17 +51,19 @@ const showLoading = async (targetId) => {
 
 const updateContent = async (apiData) => {
   for (let entry of apiData) {
-    console.log(entry);
     const contentTarget = document.getElementById(`${entry.target}-text`);
-    contentTarget.innerHTML = entry.content;
+    contentTarget.innerHTML = entry.content.replace(/\n/g, '<br />');
   }
 };
 const buildPage = async () => {
   await showLoading('other');
   await showLoading('about');
+  await showLoading('album');
   fetch('./API/home.php')
     .then((res) => res.json())
     .then((data) => updateContent(data));
 };
 
+// run the page
+buildBackgrounds();
 buildPage();
