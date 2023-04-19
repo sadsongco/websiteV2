1361f4a249b16ae43acbb710436b0ea8b5788b18
Nigel Powell <nigel@thesadsongco.com>
2023-04-20 00:58:42 +0100

    Stopped vertical scroll, added webkit

--- API/home.php
+++ API/home.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once("../../secure/scripts/teo_connect.php");
+
 $output = array(
     array(
         "target" => "other",
@@ -15,12 +17,14 @@ $output = array(
     )
 );
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS, post, get');
-header('Access-Control-Max-Age', '3600');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-header('Access-Control-Allow-Credentials', 'true');
+// header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS, post, get');
+// header('Access-Control-Max-Age', '3600');
+// header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+// header('Access-Control-Allow-Credentials', 'true');
 
 echo json_encode($output);
+
+require_once("../../secure/scripts/teo_disconnect.php");
 
 ?>
