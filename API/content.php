51ebe50723e1462430390601e8a6f650fd359cd6
Nigel Powell <nigel@thesadsongco.com>
2023-05-19 14:26:18 +0100

    Gigs first draft

--- API/content.php
+++ API/content.php
@@ -2,8 +2,26 @@
 
 require_once("../../../secure/scripts/teo_connect.php");
 
+function getGigs ($db) {
+    try {
+        $gig_stmt = $db->prepare("SELECT * FROM Gigs WHERE DATE(date) > CURDATE();");
+        $gig_stmt->execute();
+        $gig_result = $gig_stmt->fetchAll(PDO::FETCH_ASSOC);
+        if (sizeof($gig_result) > 0)
+            return $gig_result;
+        else
+            return null;
+    }
+    catch (PDOException $e) {
+        throw $e;
+    }
+}
+
 function getSingleContent($db, $card_id) {
     try {
+        if ($card_id == 'gigs') {
+            return getGigs($db);
+        }
         $stmt = $db->prepare("SELECT article_content FROM Articles WHERE card_id = ? ORDER BY live_date DESC LIMIT 1;");
         $stmt->execute(array($card_id));
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -45,14 +63,14 @@ function get_cards($db) {
                     $row['content'] = getSingleContent($db, $row['card_id']);
                     array_push($output, $row);
                     break;
-                    case 'multi-inline':
-                    case 'multi-paged':
-                        $row['content'] = getMultiContent($db, $row['card_id']);
-                        array_push($output, $row);
-                        break;
-                    }
+                case 'multi-inline':
+                case 'multi-paged':
+                    $row['content'] = getMultiContent($db, $row['card_id']);
+                    array_push($output, $row);
+                    break;
                 }
             }
+        }
         
         catch(PDOException $e) {
             echo "Database Error: " . $e->getMessage();
--- js/modules/createCard.js
+++ js/modules/createCard.js
@@ -12,7 +12,40 @@ export const createCard = (template, cardData) => {
   strap.id = `${cardData.card_id}-strap`;
   const articleEl = contentEl.getElementsByClassName('card-article')[0];
   articleEl.remove();
-  if (cardData.content) {
+  if (cardData.card_id == 'gigs') {
+    if (!cardData.content || cardData.content.length === 0) {
+      const nothing = document.createElement('p');
+      nothing.innerHTML = 'No gigs currently in the diary';
+      contentEl.appendChild(nothing);
+    } else {
+      const headings = ['date', 'venue', 'city'];
+      const table = document.getElementById('gigTable');
+      const hRow = document.getElementById('gigHead');
+      const row = document.getElementById('gigRow');
+      const hCell = hRow.firstElementChild;
+      const cell = row.firstElementChild;
+      hRow.innerHTML = '';
+      for (let heading of headings) {
+        const thisHeaderCell = hCell.cloneNode();
+        thisHeaderCell.innerHTML = heading;
+        hRow.appendChild(thisHeaderCell);
+      }
+      table.appendChild(hRow);
+      table.classList.remove('hidden');
+      for (let gig of cardData.content) {
+        let thisRow = row.cloneNode();
+        thisRow.id = gig.date;
+        for (let heading of headings) {
+          const thisCell = cell.cloneNode();
+          thisCell.innerHTML = gig[heading];
+          thisRow.appendChild(thisCell);
+        }
+        table.appendChild(thisRow);
+      }
+      contentEl.appendChild(table);
+    }
+  }
+  if (cardData.content && cardData.card_id != 'gigs') {
     cardData.content.forEach((article) => {
       const thisArticleEl = articleEl.cloneNode();
       let dateString = '';
