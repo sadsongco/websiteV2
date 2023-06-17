59a7202b3b21638cc66db7a8a63e90ef4a869239
Nigel Powell <nigel@thesadsongco.com>
2023-06-17 16:01:45 -0600

    Mailout https bug

--- email_management/includes/replace_tags.php
+++ email_management/includes/replace_tags.php
@@ -2,7 +2,8 @@
 
 function replace_tags($body_template, $row) {
     $row['secure_id'] = $row['check'];
-    $protocol = strtolower(substr($_SERVER["SERVER_PROTOCOL"],0,5))=='https'?'https':'http';
+    $protocol = 'http';
+    if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') $protocol .= 's';
     $row['host'] = "$protocol://".$_SERVER['HTTP_HOST'];
     foreach ($row as $tag_name=>$tag_content) {
         if ($tag_name == 'name' && $tag_content == '') $tag_content = 'Music Friend';
--- private/mailout/includes/replace_tags.php
+++ private/mailout/includes/replace_tags.php
@@ -3,7 +3,8 @@
 function replace_tags($body_template, $row) {
     $secure_id = hash('ripemd128', $row['email'].$row['email_id'].'JamieAndNigel');
     $row['secure_id'] = $secure_id;
-    $protocol = strtolower(substr($_SERVER["SERVER_PROTOCOL"],0,5))=='https'?'https':'http';
+    $protocol = 'http';
+    if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') $protocol .= 's';
     $row['host'] = "$protocol://".$_SERVER['HTTP_HOST'];
     foreach ($row as $tag_name=>$tag_content) {
         $body_template = str_replace("<!--{{".$tag_name."}}-->", $tag_content, $body_template);
