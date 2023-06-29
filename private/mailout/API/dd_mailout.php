3cac1dc519f9527b2c36f94efb6bb6262c2e2bb8
Nigel Powell <nigel@thesadsongco.com>
2023-06-29 18:15:43 -0400

    Nigel only version

--- private/mailout/API/dd_mailout.php
+++ private/mailout/API/dd_mailout.php
@@ -39,6 +39,7 @@ function get_email_addresses($db, $mailout_id, $log_fp) {
         $query = "SELECT email, email_id
         FROM $mailing_table
         WHERE last_sent < ?
+        AND email LIKE '%thesadsongco.com%'
         AND subscribed = 1
         AND error = 0
         LIMIT 1";
