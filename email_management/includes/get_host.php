a9420b23442ac515ce9e348d0392d3b6aead13a5
Nigel Powell <nigel@thesadsongco.com>
2024-03-12 14:59:55 +0000

    getHost issue

--- email_management/includes/get_host.php
+++ email_management/includes/get_host.php
@@ -3,7 +3,7 @@
 function getHost() {
     /**r eturn complete URL of server */
     $protocol = strtolower(substr($_SERVER["SERVER_PROTOCOL"],0,5))=='https'?'https':'http';
-    $server = !isset($_SERVER['HTTP_HOST']) || $_SERVER['HTTP_HOST'] == ''? "https://theexactopposite.uk":$_SERVER['HTTP_HOST'];
+    $server = !isset($_SERVER['HTTP_HOST']) || $_SERVER['HTTP_HOST'] == '' ? "theexactopposite.uk" : $_SERVER['HTTP_HOST'];
     return "$protocol://$server";
 }
 
