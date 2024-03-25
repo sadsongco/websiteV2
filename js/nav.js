8411478adbe65443b4b8f704be809442f50b7397
Nigel Powell <nigel@thesadsongco.com>
2024-03-25 11:46:32 +0000

    Deep linking fixed

--- js/nav.js
+++ js/nav.js
@@ -23,21 +23,31 @@ const activateSelector = (id) => {
 
 let cardsInitPos = {};
 const cardPosTolerance = 15;
-let detectScroll = true;
+let detectScroll = false;
+setTimeout(() => (detectScroll = true), 700);
 
 const getCardsInitPos = () => {
-  const params = new URLSearchParams(document.location.search);
-  const page = params.get('page');
-  if (page != '' && cardsInitPos.hasOwnProperty(page)) window.scrollTo({ top: cardsInitPos[page], behavior: 'smooth' });
-  else window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
   const cards = document.querySelectorAll('.card');
   for (const card of cards) {
     cardsInitPos[card.id] = card.offsetTop;
   }
+  const params = new URLSearchParams(document.location.search);
+  const page = params.get('page');
+  if (page != '' && cardsInitPos.hasOwnProperty(page)) {
+    document.getElementById(page).scrollIntoView({ behavior: 'smooth' });
+    activateSelector(page);
+  } else {
+    detectScroll = false;
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+    setTimeout(() => (detectScroll = true), 700);
+    return;
+  }
+
   activateNearestSelector();
 };
 
 const activateNearestSelector = () => {
+  if (!detectScroll) return;
   for (const [id, pos] of Object.entries(cardsInitPos)) {
     if (window.scrollY > pos - cardPosTolerance && window.scrollY < pos + cardPosTolerance) {
       activateSelector(id);
